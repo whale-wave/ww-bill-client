493157d2e427492349018e4bb4f4ee79eaad71ed
Avan <layouwen@gmail.com>
2024-06-14 18:08:22 +0800

    refactor: use antfu eslint config

--- .prettierrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  trailingComma: 'all', // typescript没有尾随逗号
-  tabWidth: 2,
-  semi: true, // 使用分号
-  singleQuote: true, // 使用单引号
-  jsxSingleQuote: false, // jsx使用双引号
-};
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,11 @@ import './i18n';
 import './index.scss';
 import './components/reset.scss';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { router } from './router';
 
 export const queryClient = new QueryClient();
 
-// eslint-disable-next-line react/no-deprecated
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
