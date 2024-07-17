a9244a866b7bd00463dfb690797c73793e5b82b4
Avan <layouwen@gmail.com>
2024-07-18 00:36:28 +0800

    chore: use pkg version

--- src/pages/mine/components/BotomList/BottomList.tsx
+++ src/pages/mine/components/BotomList/BottomList.tsx
@@ -2,6 +2,7 @@ import { List } from 'antd-mobile';
 import React, { useEffect, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { EditSOutline, ExclamationCircleOutline, SetOutline } from 'antd-mobile-icons';
+import pkg from '../../../../../package.json';
 
 interface BottomListProps {
 }
@@ -22,7 +23,7 @@ const BottomList: React.FC<BottomListProps> = () => {
         prefix: <div className="flex items-center text-[20px]"><EditSOutline /></div>,
       },
       {
-        title: `关于鲸浪记账 v${import.meta.env.VITE_APP_VERSION}`,
+        title: `关于鲸浪记账 v${pkg.version}`,
         prefix: <div className="flex items-center text-[18px]"><ExclamationCircleOutline /></div>,
       },
     ];
