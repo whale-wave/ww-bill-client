d6b3629f34cbf30d58587b39615156cf6d56ba02
Avan <layouwen@gmail.com>
2024-07-17 22:56:12 +0800

    chore: script

--- scripts/dev.cjs
+++ scripts/dev.cjs
@@ -1,4 +1,4 @@
 const { execSync } = require('node:child_process');
 const pkg = require('../package.json');
 
-execSync(`VITE_APP_VERSION=${pkg.version} vite --host 0.0.0.0 --port 3231`, { stdio: 'inherit' });
+execSync(`cross-env VITE_APP_VERSION=${pkg.version} vite --host 0.0.0.0 --port 3231`, { stdio: 'inherit' });
