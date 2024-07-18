9319d77399784b5621e0034e03102fa685082ae6
Avan <layouwen@gmail.com>
2024-07-18 18:03:49 +0800

    refactor: message empty todo

--- src/pages/Message/index.tsx
+++ src/pages/Message/index.tsx
@@ -1,32 +1,12 @@
-import { Icon, NavBar } from 'bw-mobile';
-import { FC } from 'react';
+import { NavBar } from 'bw-mobile';
+import type { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { ErrorBlock } from 'antd-mobile';
 import styles from './index.module.scss';
 
 const Message: FC = () => {
   const navigate = useNavigate();
-  const itemList = [
-    {
-      title: '系统通知',
-      backgroundColor: '#F0A83B',
-      icon: 'message',
-      onClick: () => goTo('/message/system-notify'),
-    },
-    {
-      title: '评论',
-      icon: 'comment',
-      backgroundColor: '#63A2EB',
-      onClick: () => goTo('/message/comment-list'),
-    },
-    {
-      title: '新增关注',
-      icon: 'follow',
-      backgroundColor: '#77BFC0',
-      onClick: () => goTo('/message/new-follow'),
-    },
-  ];
 
-  const goTo = (path: string) => navigate(path);
   return (
     <div className="page">
       <NavBar
@@ -36,33 +16,9 @@ const Message: FC = () => {
       >
         消息
       </NavBar>
-      <div className={styles.list}>
-        {itemList.map((i) => (
-          <Item key={i.title} {...i} />
-        ))}
+      <div className="flex-grow flex justify-center items-center">
+        <ErrorBlock status="empty" />
       </div>
-    </div>
-  );
-};
-
-interface ItemProps {
-  title: string;
-  backgroundColor: string;
-  icon: string;
-  onClick?: () => void;
-}
-
-const Item: FC<ItemProps> = (p) => {
-  const styleComputed = ({ backgroundColor }: ItemProps) => {
-    return { backgroundColor };
-  };
-  return (
-    <div className={styles.item} onClick={p.onClick}>
-      <div className={styles.img} style={styleComputed(p)}>
-        <Icon name={p.icon} style={{ fontSize: 24, color: 'fff' }} />
-      </div>
-      <div className={styles.title}>{p.title}</div>
-      <Icon name="right" />
     </div>
   );
 };
