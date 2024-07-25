6610759f71bcb67a6f8e52653f116c9c58b254fe
Avan <layouwen@gmail.com>
2024-07-25 23:37:02 +0800

    feat: add antd chart v2

--- src/pages/Budget/index.tsx
+++ src/pages/Budget/index.tsx
@@ -1,6 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { memo, useEffect } from 'react';
 import classNames from 'classnames';
+import { Tiny } from '@ant-design/charts';
 import { NavBar } from '@/components';
+
+const THEME_COLOR = '#aeeeff';
 
 enum BudgetItemType {
   ALL,
@@ -12,17 +15,49 @@ export interface BudgetItemProps {
   type?: BudgetItemType;
 }
 
-export const BudgetItem: React.FC<BudgetItemProps> = ({ type = BudgetItemType.ALL, className }) => {
+export const BudgetItem: React.FC<BudgetItemProps> = memo(({ type = BudgetItemType.ALL, className }) => {
   const isAll = type === BudgetItemType.ALL;
 
+  const config = {
+    height: 100,
+    width: 100,
+    padding: isAll ? 10 : 15,
+    percent: 0.7,
+    autoFit: true,
+    color: ['#f2f2f2', THEME_COLOR],
+    annotations: [
+      {
+        type: 'text',
+        style: {
+          text: `剩余`,
+          x: '50%',
+          y: '38%',
+          textAlign: 'center',
+          fontSize: isAll ? 12 : 11,
+          color: 'red',
+        },
+      },
+      {
+        type: 'text',
+        style: {
+          text: `100%`,
+          x: '50%',
+          y: '62%',
+          textAlign: 'center',
+          fontSize: isAll ? 14 : 13,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className={classNames('flex-shrink-0 bg-[#fff] h-28 flex flex-col', className)}>
+    <div className={classNames('flex-shrink-0 bg-[#fff] flex flex-col', className)}>
       <div className="flex items-center justify-between flex-shrink-0">
         <div>{ isAll ? '07月总预算' : '餐饮'}</div>
         <div>编辑</div>
       </div>
-      <div className="flex flex-grow">
-        <div>chart</div>
+      <div className="flex flex-grow h-[110px]">
+        <div className="flex justify-center items-center mr-3"><Tiny.Ring {...config} /></div>
         <div className="flex-grow">
           <div className="flex justify-between items-center">
             <div>剩余预算:</div>
@@ -46,7 +81,7 @@ export const BudgetItem: React.FC<BudgetItemProps> = ({ type = BudgetItemType.AL
       </div>
     </div>
   );
-};
+});
 
 interface BudgetProps {
 }
