18975243b5b015ca5d10f4fd9d6a0d0e029957c6
Avan <layouwen@gmail.com>
2024-11-21 20:08:51 +0800

    chore: todo

--- src/pages/Asset/AddAssetForm/AddAssetForm.tsx
+++ src/pages/Asset/AddAssetForm/AddAssetForm.tsx
@@ -1,29 +1,75 @@
 import { type FC, useCallback } from 'react';
 import { Button, Form, Input } from 'antd-mobile';
 import { useSearchParams } from 'react-router-dom';
+import { clone } from 'lodash-es';
 import { NavBar } from '@/components';
+
+// interface FormData {
+//   name: string;
+//   amount: string;
+//   remark?: string;
+// }
+
+function parseAmountString(value: string) {
+  return String(Number(value));
+}
 
 const AddAssetForm: FC = () => {
   const [query] = useSearchParams();
   const groupId = query.get('groupId') || null;
   const title = query.get('title') || '现金';
 
+  const formDataKeyParse = {
+    amount: parseAmountString,
+  };
+
   const formConfig = [
     {
       label: '名称',
-      readonly: true,
+      name: 'name',
+      // readonly: true,
     },
     {
       label: '备注',
       placeholder: '(选填)',
+      name: 'remark',
     },
     {
       label: '金额',
+      name: 'amount',
+      normalize: (value: string, preValue: string) => {
+        let normalizedValue = value.replace(/[^\d.]/g, ''); // Remove non-numeric and non-dot characters
+        const parts = normalizedValue.split('.');
+
+        if (parts.length > 2) {
+          return preValue;
+        }
+
+        if (parts[1]?.length > 2) {
+          return preValue;
+        }
+
+        if (parts[0].length > 1 && parts[0].startsWith('0')) {
+          parts[0] = parts[0].replace(/^0+/, '');
+          if (parts[0] === '') {
+            parts[0] = '0';
+          }
+          normalizedValue = parts.join('.');
+        }
+
+        return normalizedValue;
+      },
     },
   ];
 
-  const handleSave = useCallback(() => {
-    console.info(groupId);
+  const handleSave = useCallback((_formData: any) => {
+    const formData = clone(_formData);
+    Object.keys(formDataKeyParse).forEach((key) => {
+      if (formData[key]) {
+        formData[key] = formDataKeyParse[key as keyof typeof formDataKeyParse](formData[key]);
+      }
+    });
+    console.info(groupId, formData);
   }, []);
 
   return (
@@ -32,18 +78,23 @@ const AddAssetForm: FC = () => {
         添加
         {title}
       </NavBar>
-      <Form className="mt-2">
+      <Form
+        className="mt-2"
+        onFinish={handleSave}
+        footer={(<Button type="submit" block color="primary">保存</Button>)}
+      >
         {
           formConfig.map(item => (
-            <Form.Item layout="horizontal" label={item.label} key={item.label}>
-              <Input style={{ '--text-align': 'right' }} placeholder={item.placeholder} readOnly={!!item.readonly} />
+            <Form.Item layout="horizontal" label={item.label} key={item.label} name={item.name} normalize={item.normalize}>
+              <Input
+                style={{ '--text-align': 'right' }}
+                placeholder={item.placeholder}
+                // readOnly={!!item.readonly}
+              />
             </Form.Item>
           ))
         }
       </Form>
-      <div className="p-5">
-        <Button block color="primary" onClick={handleSave}>保存</Button>
-      </div>
     </div>
   );
 };
