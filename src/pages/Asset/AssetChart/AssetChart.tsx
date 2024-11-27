65cc63942526f38dcc85c146d8d9185f4fcda17c
Avan <layouwen@gmail.com>
2024-11-27 18:07:38 +0800

    chore: todo update and math function

--- src/pages/Asset/AssetChart/AssetChart.tsx
+++ src/pages/Asset/AssetChart/AssetChart.tsx
@@ -1,8 +1,11 @@
 import { type FC, useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Tabs } from 'antd-mobile';
+import classNames from 'classnames';
 import { AssetTabBar } from '../AssetManager/components';
 import { AssetTrendChart, CurAssetStatus } from './components';
 import { AssetRanking } from './components/AssetRanking';
+import styles from './AssetChart.module.scss';
 import { NavBar, TabList } from '@/components';
 
 const AssetChart: FC = () => {
@@ -11,14 +14,33 @@ const AssetChart: FC = () => {
     {
       name: '资产',
       value: 'asset',
+      children: (
+        <>
+          <AssetTrendChart />
+          <CurAssetStatus />
+          <AssetRanking />
+        </>
+      ),
     },
     {
       name: '负债',
       value: 'liability',
+      children: (
+        <>
+          <AssetTrendChart />
+          <CurAssetStatus />
+          <AssetRanking />
+        </>
+      ),
     },
     {
       name: '净资产',
       value: 'net-asset',
+      children: (
+        <>
+          <AssetTrendChart />
+        </>
+      ),
     },
   ];
 
@@ -29,11 +51,11 @@ const AssetChart: FC = () => {
   }, [navigate]);
 
   return (
-    <div className="page-new pt-[45px]">
-      <NavBar className="bg-white" backArrow={false} back={false} right={<div className="text-base" onClick={onBack}>返回</div>}>
+    <div className={classNames(styles['asset-chart'], 'page-new pt-[45px]')}>
+      <NavBar className="bg-white z-10" backArrow={false} back={false} right={<div className="text-base" onClick={onBack}>返回</div>}>
         图表
       </NavBar>
-      <div className="px-2 fixed top-[45px] w-full bg-white">
+      <div className="px-2 fixed top-[45px] w-full bg-white z-10">
         <TabList
           className="w-full"
           selectValue={selectTab}
@@ -41,11 +63,17 @@ const AssetChart: FC = () => {
           onChange={setSelectTab}
         />
       </div>
-      <div className="flex-1 px-2 py-3 space-y-3 overflow-y-auto mt-[40px] mb-[50px]">
-        <AssetTrendChart />
-        <CurAssetStatus />
-        <AssetRanking />
-      </div>
+      <Tabs onChange={setSelectTab} activeKey={selectTab}>
+        {
+          tabs.map(tab => (
+            <Tabs.Tab key={tab.value} title={tab.name}>
+              <div className="flex-1 px-2 py-3 space-y-3 overflow-y-auto mt-[40px] mb-[50px]">
+                {tab.children}
+              </div>
+            </Tabs.Tab>
+          ))
+        }
+      </Tabs>
       <AssetTabBar activeKey="chart" />
     </div>
   );
