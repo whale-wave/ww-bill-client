874ee68b1230ce8da2a2263169ea5b475d62aa80
Avan <layouwen@gmail.com>
2024-12-28 19:09:53 +0800

    chore: add @tanstack/query

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,7 @@
 import antfu from '@antfu/eslint-config';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const compat = new FlatCompat();
 
 export default antfu({
   formatters: {
@@ -11,7 +14,10 @@ export default antfu({
   },
   react: true,
   ignores: ['tsconfig.app.json'],
-}, {
+}, ...compat.config({
+  plugins: ['@tanstack/query'],
+  extends: ['plugin:@tanstack/eslint-plugin-query/recommended'],
+}), {
   rules: {
     'style/semi': ['error', 'always'],
     'style/member-delimiter-style': ['error', {
