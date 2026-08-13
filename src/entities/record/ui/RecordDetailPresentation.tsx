50abaab16e2532bd9099043a5a79fd1e456bab42
Avan <layouwen@gmail.com>
2026-08-13 13:17:06 +0800

    fix: edit padding

--- src/entities/record/ui/RecordDetailPresentation.tsx
+++ src/entities/record/ui/RecordDetailPresentation.tsx
@@ -47,7 +47,7 @@ export const RecordDetailPresentation: FC<RecordDetailPresentationProps> = ({
   return (
     <div className="page" data-record-detail-presentation>
       {showNavigation && <NavBar back={backLabel} backArrow={false} onBack={onBack} />}
-      <div className="relative h-[96px] w-full shrink-0 bg-primary" data-record-detail-header>
+      <div className="relative h-[130px] w-full shrink-0 bg-primary" data-record-detail-header>
         <div
           className="absolute left-1/2 top-[8px] z-10 -translate-x-1/2 text-center"
           data-record-detail-category
